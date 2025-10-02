bbbef8fe421ec26f59760b742c472798206c3a8d
Biju Mondal <biju.m@op3n.world>
2025-10-02 12:13:01 +0530

    force update dialog updated

--- lib/presentation/widgets/show_force_update_dialog.dart
+++ lib/presentation/widgets/show_force_update_dialog.dart
@@ -1,8 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-void showUpdateDialog(BuildContext context, {
+import '../../core/themes/app_colors.dart';
+import '../../core/themes/app_sizes.dart';
+
+void showUpdateDialog(
+  BuildContext context, {
   required bool forceUpdate,
   required String updateUrl,
 }) {
@@ -10,15 +13,30 @@ void showUpdateDialog(BuildContext context, {
     barrierDismissible: !forceUpdate,
     context: context,
     builder: (context) => AlertDialog(
-      title: const Text('Update Available'),
-      content: Text(forceUpdate
-          ? 'A new version of the app is required to continue.'
-          : 'A new version of the app is available. Would you like to update?'),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppSizes.cardCornerRadius),
+      ),
+      backgroundColor: Theme.of(context).cardColor,
+      title: Text(
+        'Update Available',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+      content: Text(
+        forceUpdate
+            ? 'A new version of the app is required to continue.'
+            : 'A new version of the app is available. Would you like to update?',
+        style: Theme.of(context).textTheme.bodyLarge,
+      ),
       actions: [
         if (!forceUpdate)
           TextButton(
             onPressed: () => Navigator.pop(context),
-            child: const Text('Later'),
+            child: Text(
+              'Later',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.surfaceContainer,
+              ),
+            ),
           ),
         TextButton(
           onPressed: () async {
@@ -27,7 +45,12 @@ void showUpdateDialog(BuildContext context, {
               await launchUrl(uri, mode: LaunchMode.externalApplication);
             }
           },
-          child: const Text('Update'),
+          child: Text(
+            'Update',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
         ),
       ],
     ),
