b4372fe55198782166d2088a67ac28d11ea10868
Biju Mondal <biju.m@op3n.world>
2025-10-02 12:09:56 +0530

    repository created

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -3,6 +3,8 @@ class ApiEndpoints {
   static const String loginURL = '/auth/login';
   static const String forgotPasswordURL = '/auth/forgot_password';
   static const String verifyAccountURL = '/auth/verify_account';
+  static const String fetchUserProfileURL = '/users/profile';
+  static const String uploadProfilePhotoURL = '/users/upload_profile_photo';
 
   // ✅ ROUTE API Endpoints
   static const String fetchAllRoutesURL = '/routes/all_routes';
