5b93ff847499cddaf93f2be4fc835f9a93456e1f
Biju Mondal <biju.m@op3n.world>
2025-10-24 20:52:19 +0530

    fixed.

--- lib/presentation/pages/available_routes/available_routes_screen.dart
+++ lib/presentation/pages/available_routes/available_routes_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:navex/presentation/pages/home/components/build_upcoming_route_list.dart';
 
 import '../../../core/themes/app_colors.dart';
 import '../../../core/themes/app_sizes.dart';
 import '../../../core/utils/date_time_utils.dart';
+import '../../../data/repositories/route_repository.dart';
+import '../../bloc/route_bloc.dart';
 import '../../widgets/route_card.dart';
 
 class AvailableRoutesScreen extends StatefulWidget {
@@ -14,16 +17,17 @@ class AvailableRoutesScreen extends StatefulWidget {
 }
 
 class _AvailableRoutesScreenState extends State<AvailableRoutesScreen> {
-
   DateTime? _selectedDate;
 
   Future<void> _openCalendar() async {
     final DateTime? pickedDate = await showDatePicker(
       context: context,
       barrierDismissible: false,
-      initialDate: _selectedDate ?? DateTime.now(), // current date
-      firstDate: DateTime.now(),   // no previous date allowed
-      lastDate: DateTime(2100),    // latest date allowed
+      initialDate: _selectedDate ?? DateTime.now(),
+      // current date
+      firstDate: DateTime.now(),
+      // no previous date allowed
+      lastDate: DateTime(2100), // latest date allowed
     );
 
     if (pickedDate != null && pickedDate != _selectedDate) {
@@ -33,63 +37,69 @@ class _AvailableRoutesScreenState extends State<AvailableRoutesScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.2,
-            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-            child: Padding(
-              padding: const EdgeInsets.all(AppSizes.kDefaultPadding),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Available Routes',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.titleLarge!.copyWith(color: AppColors.white),
-                  ),
-                  GestureDetector(
-                    onTap: () => _openCalendar(),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: AppSizes.kDefaultPadding,
-                        vertical: AppSizes.kDefaultPadding / 4,
-                      ),
-                      decoration: BoxDecoration(
-                        color: AppColors.scaffoldBackgroundDark.withAlpha(100),
-                        borderRadius: BorderRadius.circular(
-                          AppSizes.cardCornerRadius * 5,
+    return BlocProvider(
+      create: (context) => RouteBloc(RouteRepository()),
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.2,
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+              child: Padding(
+                padding: const EdgeInsets.all(AppSizes.kDefaultPadding),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Available Routes',
+                      style: Theme.of(
+                        context,
+                      ).textTheme.titleLarge!.copyWith(color: AppColors.white),
+                    ),
+                    GestureDetector(
+                      onTap: () => _openCalendar(),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: AppSizes.kDefaultPadding,
+                          vertical: AppSizes.kDefaultPadding / 4,
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColors.scaffoldBackgroundDark.withAlpha(
+                            100,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            AppSizes.cardCornerRadius * 5,
+                          ),
+                        ),
+                        child: Text(
+                          DateTimeUtils.getFormattedSelectedDate(
+                                _selectedDate ?? DateTime.now(),
+                              ) ??
+                              DateTimeUtils.getFormattedCurrentDate(),
+                          style: Theme.of(context).textTheme.bodyLarge!
+                              .copyWith(color: AppColors.white),
                         ),
                       ),
-                      child: Text(
-                       DateTimeUtils.getFormattedSelectedDate(_selectedDate?? DateTime.now()) ?? DateTimeUtils.getFormattedCurrentDate(),
-                        style: Theme.of(
-                          context,
-                        ).textTheme.bodyLarge!.copyWith(color: AppColors.white),
-                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
 
-          Positioned(
-            top: 70,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            child: BuildUpcomingRouteList(pickedDate: _selectedDate,),
-          ),
-        ],
+            Positioned(
+              top: 70,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              child: BuildUpcomingRouteList(pickedDate: _selectedDate),
+            ),
+          ],
+        ),
       ),
     );
   }
